38fb4eb7fc74ba33b9c2e35b552495c12f879c14
shonenA <hitel00000@hotmail.com>
2013-03-08 12:21:33 +0900

    디버그 메시지 추가

--- musiclist.js
+++ musiclist.js
@@ -8,10 +8,12 @@ var eagatedata = [];
 c.getEagateMusicList();
 
 c.on('music.data', function(name, url) {
+    console.log('eagate:' + name);
     eagatedata.push({name:name,url:url});
 });
 
 c.on('music.end', function() {
+    console.log('eagate.end');
     fs.writeFile('eagate.json', JSON.stringify(eagatedata));
 });
 
@@ -21,10 +23,12 @@ var atwikidata = [];
 c.getAtwikiMusicList();
 
 c.on('atwiki.data', function(name, artist, bpm, difficulty, notecount) {
+    console.log('atwiki:' + name);
     atwikidata.push({name:name,artist:artist,bpm:bpm,difficulty:difficulty,notecount:notecount});
 });
 
 c.on('atwiki.end', function() {
+    console.log('atwiki.end');
     fs.writeFile('atwiki.json', JSON.stringify(atwikidata));
 });
 
